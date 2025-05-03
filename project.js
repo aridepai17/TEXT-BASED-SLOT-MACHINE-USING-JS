2d3bee80b2455d6181c01376e2671802cdd970b3
aridepai17 <advaithdepai26@gmail.com>
2025-05-03 13:46:50 +0530

    Spinning the slot machine and displaying the reels

--- project.js
+++ project.js
@@ -7,6 +7,24 @@
 // Play again
 
 const prompt = require('prompt-sync')();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8,
+}
+
+const SYMBOLS_VALUES = {
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2,
+}
+
 const deposit = () => {
     while (true) {
         const depositAmount = prompt('Enter deposit amount: ');
@@ -44,6 +62,57 @@ const getBet = (balance, lines) => {
     }
 };
 
+const spin = () => {
+    const symbols = [];
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for( let i = 0; i < count; i++){
+            symbols.push(symbol);
+        }
+    }
+
+    const reels = [[], [], []];
+    for (let i = 0; i < COLS; i++){
+        reels.push([]);
+        const reelSymbols = [...symbols];
+        for (let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1);
+        }
+    }
+    return reels;
+};
+
+const transpose = (reels) => {
+    const rows = [];
+
+    for (let i = 0; i < ROWS; i++){
+        rows.push([]);
+        for ( let j = 0; j < COLS; j++){
+            rows[i].push(reels[j][i]);
+        }
+    }
+    return rows;
+}
+
+const printRows = (rows) => {
+    for (const row of rows){
+        let rowString = '';
+        for (const [i, symbol] of row.entries()){
+            rowString += symbol;
+            if (i !== row.length - 1){
+                rowString += ' | ';
+            }
+        }
+        console.log(rowString);
+    }
+}
+
+
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
+const reels = spin();
+const rows = transpose(reels);
+printRows(rows);
